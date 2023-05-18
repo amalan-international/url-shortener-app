a10fde4f51839f2cbccf74c1950ff462ec4efd0d
Yuda Yahya <yudayahya7@gmail.com>
2023-05-19 05:09:21 +0700

    replace the content

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -12,7 +12,7 @@
 			<a href="/" class="btn variant-filled"><i class="ri-arrow-left-line mr-1" /> Back</a>
 		</header>
 		<section class="p-4 h-4/6 flex justify-center items-center">
-			<div class="w-full max-w-md">-</div>
+			<div class="w-full max-w-md text-center">Content will available soon.</div>
 		</section>
 		<footer class="card-footer h-1/6 flex justify-center items-end">
 			<Copyright />
